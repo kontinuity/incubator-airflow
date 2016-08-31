e7d9fc2b8e929a4da60f1cf188e2f0180ad95b3d
Arif Amirani <arif@metroguild.com>
2016-08-31 12:12:53 +0530

    Do not create empty log directories if common log is enabled

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -190,16 +190,18 @@ def run(args, dag=None):
 
     # Setting up logging
     log_base = os.path.expanduser(conf.get('core', 'BASE_LOG_FOLDER'))
-    directory = log_base + "/{args.dag_id}/{args.task_id}".format(args=args)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not conf.get('core', 'COMMON_LOG_FILENAME'):
+        directory = log_base + "/{args.dag_id}/{args.task_id}".format(args=args)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
     iso = args.execution_date.isoformat()
     filename = "{directory}/{iso}".format(**locals())
     log_format = settings.LOG_FORMAT
 
     common_log_filename = os.path.join(log_base, conf.get('core', 'COMMON_LOG_FILENAME'))
     if common_log_filename:
-        prefix = '{}/{}'.format(args.dag_id, iso) if dag else iso
+        prefix = '{}/{}'.format(args.dag_id, iso) if 'dag_id' in args else iso
         log_format = '{} - {}'.format(prefix, log_format)
         filename = common_log_filename
 
